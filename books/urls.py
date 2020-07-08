ca7e0c06b14b3ba80f251c513a97431b6a92b477
CSJ516 <dalgoon02121@naver.com>
2020-07-08 11:46:24 +0900

    modified

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from books import views
 
 app_name = 'books'
 
 urlpatterns = [
-    
+    # books/
+    path('', views.BookList.as_view(), name='list'),
+    # books/도서ID/
+    path('<int:pk>/', views.BookDetailView.as_view(), name='detail'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
+from books.models import BookCategory, Publisher, Book
+
+class BookList(generic.ListView):
+    model = Book
+    template_name = 'books/list.html'
+    context_object_name = 'total_book_list'
+
+class BookDetailView(generic.DetailView):
+    # queryset = Book.objects.all()
+    template_name = 'books/detail.html'
+    context_object_name = 'book_detail'
