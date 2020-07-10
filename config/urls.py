00eeb1c2daf885dc6f2542ab7aab3ec70e62f312
kwak se jin <Reblackraven@gmail.com>
2020-07-10 16:35:07 +0900

    merge authors and writerblogs

--- config/urls.py
+++ config/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("writerblog/", include("writerblogs.urls"), name="writerblog",),
     path("author/", include("authors.urls"), name="author"),
-    # path("articles/", include("articles.url"))
+    path("articles/", include("articles.url"))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
